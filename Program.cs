cdbbf7a96d70391982923fb21abba9de38f5bccf
gusus21 <yanenko.yurii1121@vu.cdu.edu.ua>
2021-12-11 18:53:42 +0200

    Update Code(new main and method)

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Lab_5
 {
     class Program
     {
-        static void Random()
+        static void Randomfill()
         {
             Console.WriteLine("Please, enter the amount of the array numbers:\t");
             int amountOfArrayNumbers = int.Parse(Console.ReadLine());
@@ -17,15 +17,16 @@ namespace Lab_5
             Console.WriteLine("Enter the max value of the array:\t");
             int maxValue = int.Parse(Console.ReadLine());
             Random ran = new Random();
-            int[] b = new int[amountOfArrayNumbers];
-            for (int i = 0; i < b.Length; i++)
+            int[] myArray = new int[amountOfArrayNumbers];
+            Console.WriteLine("Your array:");
+            for (int i = 0; i < myArray.Length; i++)
             {
-                Console.WriteLine("Your array:");
-                b[i] = ran.Next(minValue, maxValue);
-                Console.Write(b[i] + " ");
+                myArray[i] = ran.Next(minValue, maxValue);
+                Console.Write(myArray[i] + " ");                
             }
+            Console.ReadLine();
         }
-        static void Handmade()
+        static void HandmadeEnter()
         {
             Console.WriteLine("Please, enter the amount of the array numbers:\t");
             int n = int.Parse(Console.ReadLine());
@@ -41,20 +42,43 @@ namespace Lab_5
                 Console.WriteLine(myArray[j]);
             }
         }
+        static void HandmadeSpaces()
+        {
+            string[] data = Console.ReadLine().Trim().Split();
+            int[] myArray;
+            
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int[] b = new int[n];          
-            for (int i = 0; i < b.Length; i++)
-            {                
-                b[i] = int.Parse(Console.ReadLine());                
-            }
-            Console.WriteLine("Введений вами масив:");
-            for (int j = 0; j < b.Length; j++)
+            Console.WriteLine("What type of array input you want(1-hand; 2-random)");
+            int choise = int.Parse(Console.ReadLine());
+            switch (choise)
             {
-                Console.WriteLine(b[j]);
+                case 1:
+                    {
+                        string ch = Console.ReadLine();
+                        if (ch == "enter")
+                        {
+                            HandmadeEnter();
+                        }
+                        else if (ch == "spaces")
+                            HandmadeSpaces();
+                        else
+                            Console.WriteLine("Error");
+                        break;
+                    }
+                case 2:
+                    {
+                        Randomfill();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Bruh, I say, 1 or 2. I lazzy programer and I don't want give you a chance to repeat, hehe ^_^");
+                        break;
+                    }
             }
-            Console.ReadKey();
         }
     }
 }
